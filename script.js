5e0aadd2cb068281a6813ef5d75831c754adde05
Withacker Futenma <106629705+Winntter@users.noreply.github.com>
2023-02-05 00:50:12 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const daysNumEl = document.querySelector("#days-number");
+const hoursNumEl = document.querySelector("#hours-number");
+const minutesNumEl = document.querySelector("#minutes-number");
+const secondsNumEl = document.querySelector("#seconds-number");
+
+//                 Programming Logic
+
+const newYear = "01-01-2024"
+
+function countdown() {
+    const actualDate = new Date();
+    const newYearDate = new Date(newYear);
+
+    const base = Math.floor((actualDate - newYearDate) / 1000); // Transform MS to Seconds
+
+    const days = Math.floor(base / 3600 / 24);
+    const hours = Math.floor(base / 3600) % 24;
+    const minutes = Math.floor(base / 60) % 60;
+    const seconds = Math.floor(base % 60);
+
+    daysNumEl.innerHTML = days < 10 ? `0${days}` : days;
+    hoursNumEl.innerHTML = hours < 10 ? `0${hours}` : hours;
+    minutesNumEl.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    secondsNumEl.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+    
+}
+
